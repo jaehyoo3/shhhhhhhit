ed05b4afd598c1dd29f9a374ee7c5d61d881a931
ShinJH <goxan@hallowa.co.kr>
2025-12-01 12:06:15 +0900

    개발-브랜치

--- src/main/java/com/foorend/api/common/config/DataSourceConfig.java
+++ src/main/java/com/foorend/api/common/config/DataSourceConfig.java
@@ -1,43 +1,40 @@
 package com.foorend.api.common.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 /**
- * DataSource 및 트랜잭션 설정
+ * H2 DataSource 설정 (개발/테스트용)
+ * - DataSource는 Spring Boot 자동 설정 사용
+ * - schema.sql 초기화만 별도 설정
  */
 @Configuration
 @EnableTransactionManagement
 public class DataSourceConfig {
 
-    @Bean
-    @ConfigurationProperties(prefix = "maindb")
-    public HikariConfig mainDBHikariConfig() {
-        return new HikariConfig();
-    }
-
-    @Primary
-    @Bean(name = "mainDBDataSource")
-    public DataSource mainDBDataSource() {
-        return new HikariDataSource(mainDBHikariConfig());
-    }
+    @Value("classpath:schema.sql")
+    private Resource schemaScript;
 
     /**
-     * 트랜잭션 매니저
+     * H2 DB 초기화 - 서버 시작 시 schema.sql 자동 실행
      */
-    @Primary
-    @Bean(name = "transactionManager")
-    public PlatformTransactionManager transactionManager(@Qualifier("mainDBDataSource") DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(schemaScript);
+        populator.setSqlScriptEncoding("UTF-8");
+        populator.setContinueOnError(true);
+
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(populator);
+        return initializer;
     }
 }
